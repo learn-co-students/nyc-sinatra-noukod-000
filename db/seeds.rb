f91f4a06564e98f643ce24f32e38900d10aea33b
Mac Dowell Prosper <macdowell@noukod.com>
2018-12-27 05:27:12 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,92 @@
+figure_list = {
+    "Robert Moses" => {
+    },
+    "Al Smith" => {
+    },
+    "Theodore Roosevelt" => {
+    },
+    "Peter Stuyvesant" => {
+    },
+    "Boss Tweed" => {
+    },
+    "Michael Bloomberg" => {
+    },
+    "Ed Koch" => {
+    },
+    "Fiorello LaGuardia" => {
+    },
+    "Jimmy Walker" => {
+    },
+    "Belle Moskowitz" => {
+    },
+    "Billy The Kid" => {
+    },
+    "Mark Zuckerberg" => {
+    },
+    "Ada Lovelace" => {
+    },
+    "Linus Torvalds" => {
+    }
+  }
+
+figure_list.each do |name, figure_hash|
+  p = Figure.new
+  p.name = name
+  p.save
+end
+
 landmarks_list = {
+    "BQE" => {
+      :year_completed => 1947,
+      :figure_id => 1
+    },
+    "Holland Tunnel" => {
+      :year_completed => 1927,
+      :figure_id => 5
+    },
+    "Wall Street Wall" => {
+      :year_completed => 1684,
+      :figure_id => 4
+    },
+    "Brooklyn Battery Tunnel" => {
+      :year_completed => 1973,
+      :figure_id => 10
+    },
     "Madison Square Guarden" => {
-      :year_completed => 1901
+      :year_completed => 1901,
+      :figure_id => 10
     },
     "The big bull outside" => {
-      :year_completed => 1995
+      :year_completed => 1995,
+      :figure_id => 1
     },
     "Flatiron School" => {
-      :year_completed => 2014
+      :year_completed => 2014,
+      :figure_id => 5
     },
     "Museum Mile" => {
-      :year_completed => 1058
+      :year_completed => 1058,
+      :figure_id => 4
     },
     "Subway Station" => {
-      :year_completed => 2015
+      :year_completed => 2015,
+      :figure_id => 3
     },
     "Cast Iron" => {
-      :year_completed => 1951
+      :year_completed => 1951,
+      :figure_id => 3
     },
     "Shea Stadium" => {
-      :year_completed => 1964
+      :year_completed => 1964,
+      :figure_id => 2
     },
     "Flatiron Building" => {
-      :year_completed => 1902
+      :year_completed => 1902,
+      :figure_id => 8
     },
     "Brooklyn Bridge" => {
-      :year_completed => 1883
+      :year_completed => 1883,
+      :figure_id => 6
     }
   }
 
@@ -37,25 +99,16 @@ landmarks_list.each do |name, landmark_hash|
   p.save
 end
 
-figure_list = {
-    "Billy The Kid" => {
-    },
-    "Mark Zuckerberg" => {
-    },
-    "Ada Lovelace" => {
-    },
-    "Linus Torvalds" => { 
-    }
-  }
-
-figure_list.each do |name, figure_hash|
-  p = Figure.new
-  p.name = name
-  p.save
-end
-
 
 title_list = {
+    "Mayor" => {
+    },
+    "President" => {
+    },
+    "Governor" => {
+    },
+    "NYC Parks commissioner" => {
+    },
     "The Mayor" => {
     },
     "The Don" => {
@@ -69,3 +122,35 @@ title_list.each do |name, figure_hash|
   p.name = name
   p.save
 end
+
+# figure = Figure.create(name: 'Robert Moses')
+figure_title_list = {
+    7 => {
+      :title_id => 3,
+      :figure_id => 2
+    },
+    3 => {
+      :title_id => 3,
+      :figure_id => 3
+    },
+    2 => {
+      :title_id => 2,
+      :figure_id => 3
+    },
+    1 => {
+      :title_id => 1,
+      :figure_id => 9
+    },
+    4 => {
+      :title_id => 4,
+      :figure_id => 1
+    }
+  }
+
+  figure_title_list.each do |number, figure_title_hash|
+    p = FigureTitle.new
+    figure_title_hash.each do |attribute, value|
+        p[attribute] = value
+    end
+    p.save
+  end
