4d84ce4593e5c2aa9efeab6706729517adc15ddc
Alexandro Disla <alexandrodisla@hotmail.com>
2019-08-13 16:25:07 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,34 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  post '/landmarks' do
+    #binding.pry
+    @landmarks = Landmark.create(params[:landmark]) unless params[:landmark][:name].empty?||params[:landmark][:year_completed].empty?
+    #binding.pry
+    redirect  '/landmarks'
+  end
+
+  get '/landmarks/:id' do
+    @landmarks = Landmark.find_by_id(params[:id])
+    #binding.pry
+    erb :'landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+     @landmarks = Landmark.find_by_id(params[:id])
+     erb :'landmarks/edit'
+   end
+
+   patch '/landmarks/:id' do
+    @landmarks = Landmark.find_by_id(params[:id])
+    @landmarks.update(params[:Landmark]) unless params[:Landmark][:name].empty?||params[:Landmark][:year_completed].empty?
+    redirect '/landmarks'
+   end
 end
---
+++ db/migrate/03_create_titles.rb
@@ -0,0 +1,8 @@
+class CreateTitles < ActiveRecord::Migration
+  #raise "Write CreateTitles migration here"
+  def change
+    create_table :titles do |t|
+      t.string :name
+    end
+  end
+end
