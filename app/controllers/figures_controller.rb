9cd0556cbf2bb8d2eedab454b1d03954d0c9ede5
Rachele Suy <rachelesuy@gmail.com>
2019-09-13 13:38:29 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,46 @@
 class FiguresController < ApplicationController
+  register Sinatra::ActiveRecordExtension
+   set :session_secret, "my_application_secret"
+   set :views, Proc.new { File.join(root, "../views/") }
+  
   # add controller methods
+
+  get '/figures' do
+    @figure =Figure.all
+    erb :'figures/index'
+  
+
+  get '/figures/new' do
+    erb :'figures/new'
+  end
+
+  post '/figures' do
+    #jbjbj
+    @figure = Figure.create(params['figure'])
+    unless params[:landmark][:name].empty?
+    @figure.landmarks << Landmark.create(params[:landmark])
+  end
+
+   unless params[:title][:name].empty?
+     @figure.titles << Title.create(params[:title])
+  end
+
+  @figure.save
+  redirect to "/figures/#{@figure.id}"
+  end
 end
+
+
+# post '/figures' do
+#   @figure = Figure.create(params['figure'])
+#   unless params[:landmark][:name].empty?
+#     @figure.landmarks << Landmark.create(params[:landmark])
+#   end
+
+#   unless params[:title][:name].empty?
+#     @figure.titles << Title.create(params[:title])
+#   end
+
+#   @figure.save
+#   redirect to "/figures/#{@figure.id}"
+# end
---
+++ db/migrate/20190913145639_create_landmarks.rb
@@ -0,0 +1,8 @@
+class CreateLandmarks < ActiveRecord::Migration
+  def change
+    create_table :landmarks do |t|
+      t.string :name
+      t.string :year_completed
+    end
+  end
+end
